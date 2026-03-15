fd17fc69232b4b42f29ee30d93ae8694793ed4fa
Michel Manquenir Leyton <michel.manquenir@siigroup.cl>
2026-03-15 13:07:33 -0300

    fix: correct UserDetailsServiceAutoConfiguration package for Spring Boot 4.x

--- src/main/java/barberiapp/BarberiappBackendApplication.java
+++ src/main/java/barberiapp/BarberiappBackendApplication.java
@@ -2,7 +2,7 @@ package barberiapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class BarberiappBackendApplication {
